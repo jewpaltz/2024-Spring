aa7254de74b0c9bf88e729d11d05f76d57dd8658
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-02-22 13:33:39 -0500

    file based routing

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),

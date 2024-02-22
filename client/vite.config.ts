155aed70546a1ac7be403eaebb255c02e1cab181
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-02-22 11:15:09 -0500

    scaffolds client

---
+++ client/vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+import VueRouter from 'unplugin-vue-router/vite'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter({
+      /* options */
+    }),
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})

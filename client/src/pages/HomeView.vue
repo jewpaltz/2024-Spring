12a7c13514a78dcecaf5293838876b226f7e47c2
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-02-26 14:38:10 -0500

    Cleared out boilerplate

--- client/src/pages/HomeView.vue
+++ client/src/pages/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="hero is-primary is-large">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Home</h1>
+        <h2 class="subtitle">Welcome to the Home page</h2>
+      </div>
+    </div>
   </main>
 </template>

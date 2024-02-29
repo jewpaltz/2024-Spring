8d33054786b66b8efed1d0dac37b6587514275cc
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-02-29 15:00:23 -0500

    Implemented bulma navbar collapsing

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
 
+let isActive = ref(false);
+
+function toggleMenu() {
+  isActive.value = !isActive.value;
+}
 
 </script>
 
@@ -11,14 +17,14 @@ import { RouterLink } from 'vue-router';
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
     </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" @click="toggleMenu" :class="{ 'is-active': isActive } " class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
+  <div :class="{ 'is-active': isActive } " id="navbarBasicExample" class="navbar-menu"  >
     <div class="navbar-start">
       <RouterLink to="/" class="navbar-item">
         Home
@@ -69,12 +75,9 @@ import { RouterLink } from 'vue-router';
 
 <style scoped>
 
-    .router-link-exact-active {
+    .router-link-active {
         border-bottom: 2px solid #00d1b2;
     }
 
-    .router-link-active {
-        background-color: aquamarine;
-    }
 
 </style>

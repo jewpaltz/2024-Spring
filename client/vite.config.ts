b59503594b34649f42cfbe22d6abbe691eb23a26
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-02-29 17:56:48 -0500

    using environment variables

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL,
   plugins: [
     VueRouter({
       /* options */

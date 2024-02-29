be56db1f61278c80b935cf5d7df557fdf2e72976
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-02-29 18:11:47 -0500

    checking environment variables

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 
-
+console.log(`import.meta.env.BASE_URL: ${import.meta.env.BASE_URL}`)
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes

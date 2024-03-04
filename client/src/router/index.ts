3f5bc25b040a64431ed286665d9f51107d319f7b
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-03-03 19:27:09 -0500

    vite variables

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 
 console.log(`import.meta.env.BASE_URL: ${import.meta.env.BASE_URL}`)
+console.log(`import.meta.env.S_BASE_URL: ${import.meta.env.S_BASE_URL}`)
+console.log(`import.meta.env.E_BASE_URL: ${import.meta.env.E_BASE_URL}`)
+console.log(`import.meta.env.L_BASE_URL: ${import.meta.env.L_BASE_URL}`)
+console.log(`import.meta.env.VITE_BASE_URL: ${import.meta.env.VITE_BASE_URL}`)
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes

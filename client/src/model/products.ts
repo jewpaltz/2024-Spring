35ab95ceaf0c5caff6cfd7553149482ecde74433
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-03-04 14:37:10 -0500

    Added dummy data

---
+++ client/src/model/products.ts
@@ -0,0 +1,19 @@
+import products from '@/data/products.json';
+
+export interface Product {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
+}
+
+export function getProducts(): Product[] {
+    return products.items;
+}

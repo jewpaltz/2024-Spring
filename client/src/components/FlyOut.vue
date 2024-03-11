fc3690e241e68a63a1cf742bb601a61673cb7a1c
Moshe Plotkin <mplotkin@jewpaltz.com>
2024-03-11 14:58:38 -0400

    Created Flyout Component

---
+++ client/src/components/FlyOut.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    isOpen?: boolean
+}>()
+</script>
+
+<template>
+    <div class="flyout" :class="{ open: isOpen}">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .flyout {
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 20rem;
+        height: 100%;
+        background-color: white;
+        border: 1px solid #ccc;
+        box-shadow: -1px 0 5px 0 rgba(0,0,0,.5);
+        z-index: 100;
+        transform: translateX(80%);
+        transition: transform 1s;
+        padding: 1rem;
+    }
+    .flyout.open, .flyout:hover {
+        transform: translateX(0);
+    }
+</style>
--- client/src/pages/products/index.vue
+++ client/src/pages/products/index.vue
@@ -2,6 +2,7 @@
     import { ref, computed } from 'vue'
     import { type Product, getProducts } from "@/model/products";
     import ProductCard from '@/components/ProductCard.vue';
+import FlyOut from '@/components/FlyOut.vue';
 
     const products = ref([] as Product[])
     
@@ -36,7 +37,7 @@
                     />
     </div>
 
-    <div class="flyout">
+    <FlyOut >
         <h1 class="title">
             The Cart
         </h1>
@@ -47,7 +48,7 @@
             </li>
         </ul>
         {{ cart.length }} items totalling ${{ total }}
-    </div>
+    </FlyOut>
 </template>
 
 <style scoped>
@@ -56,38 +57,6 @@
         flex-wrap: wrap;
         justify-content: space-around;
     }
-    .card {
-        flex-basis: 15rem;
-        flex-grow: 1;
-        margin: .5rem;
-    }
-    h3 {
-        font-weight: bold;
-    }
-    .price {
-        font-weight: bold;
-        font-size: xx-large;
-        color: hotpink;
-        float: right;
-    }
-
-.flyout {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 20rem;
-    height: 100%;
-    background-color: white;
-    border: 1px solid #ccc;
-    box-shadow: -1px 0 5px 0 rgba(0,0,0,.5);
-    z-index: 100;
-    transform: translateX(80%);
-    transition: transform 1s;
-    padding: 1rem;
-}
-.flyout.open, .flyout:hover {
-    transform: translateX(0);
-}
 
 .cart li {
     display: flex;
